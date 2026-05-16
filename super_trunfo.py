8b6c78970a9a8d5f30a57dafe603d41deb7fb62d
pedro_twjgqs5 <pedro_twjgqs5@PEDRO>
2026-05-16 15:42:24 -0300

    Criação do baralho

--- super_trunfo.py
+++ super_trunfo.py
@@ -5,6 +5,35 @@ Pedro Henrique Bettega Trama - RA: 10769933
 
 Turma 01D - L12
 '''
+import random
+
+baralho = [ # lista de cartas
+    ["Bimota TESI 2D", 992, 92, 6000, 8000, 149],
+    ["Ducati Monster", 1000, 98, 7700, 8800, 190],
+    ["Yamaha Fazer 600", 600, 98, 6000, 12000, 187],
+    ["BMW R 1100 S", 1085, 98, 3500, 7500, 229],
+    ["Aprilia RST Futura", 1000, 10, 7500, 9500, 210],
+    ["Honda Rc51", 1000, 160, 7000, 10000, 218],
+    ["Kawazaki Ninja ZXR", 1200, 115, 8500, 9500, 240],
+    ["Yamaha YZF-R1", 996, 130, 7500, 10500, 290],
+    ["Ducati 999", 999, 124, 8000, 9500, 195],
+    ["Hyosung GT 650", 650, 79, 6500, 9000, 189],
+    ["Bimota Db5 Mille", 992, 92, 6500, 8500, 156],
+    ["Yamaha Royal Star", 1300, 98, 4800, 6000, 366],
+    ["Bimota Santamônica", 1000, 107, 7000, 9000, 236],
+    ["Honda VTX 1800 Retro", 1800, 106, 3100, 5800, 290],
+    ["BMW K 1200", 1200, 130, 4250, 8750, 285],
+    ["Aprilia Tuono 1000", 1000, 126, 7500, 9500, 245],
+
+]
+
+cartas = random.sample(baralho, k=len(baralho)) # embaralha as cartas do jogo
+
+# divide a lista e distribui as cartas para cada jogador (sem repetições)
+mao_jogador1 = cartas[:3] 
+mao_jogador2 = cartas[3:]
+
+descarte = [] # pílha de descarte (caso haja empate)
 
 # função que exibe o menu inicial e as opções
 def menu_inicial():
---
+++ teste.py
@@ -0,0 +1,50 @@
+# super trunfo de motos - apenas 6 cartas (testes)
+import random
+
+lista = [
+    ["Hyosung GT 150", 650, 79, 6500, 9000, 189],
+    ["Bimota Santamonica", 1000, 107, 7000, 9000, 236],
+    ["Aprilia Tuono 1000", 1000, 126, 7500, 9500, 245],
+    ["Bimota Db5 Mille", 992, 92, 6500, 8500, 156],
+    ["BMW K 1200", 1200, 130, 4250, 8750, 285],
+    ["Yamaha Royal Star", 1300, 98, 4800, 6000, 366]
+]
+
+cartas = random.sample(lista, k=len(lista)) # embaralha as cartas do jogo
+
+# divide a lista e distribui as cartas para cada jogador (sem repetições)
+mao_jogador1 = cartas[:3] 
+mao_jogador2 = cartas[3:]
+
+descarte = [] # pílha de descarte (caso haja empate)
+
+print(f"Nome:           {mao_jogador1[0][0]}")
+print(f"Cilindradas:    {mao_jogador1[0][1]}")
+print(f"Potência (hp):  {mao_jogador1[0][2]}")
+print(f"Torque (rpm):   {mao_jogador1[0][3]}")
+print(f"RPM:            {mao_jogador1[0][4]}")
+print(f"Peso (kg):      {mao_jogador1[0][5]}")
+print()
+atributo_j1 = int(input("1. Cilindradas \n" \
+"2. Potência\n" \
+"3. Torque\n" \
+"4. RPM\n" \
+"5. Peso\n" \
+"Digite uma das opções: "))
+
+atributo_j2 = random.randint(1, 5) # escolha aleatória do jogador 2 
+
+# vez do jogador 1
+while len(mao_jogador1) != 0:
+    # se o atributo do jogador 1 for maior do que o do jogador 2
+    if mao_jogador1[0][atributo_j1] > mao_jogador2[0][atributo_j1]:
+        mao_jogador1.append(mao_jogador2[0])
+        mao_jogador2.pop(0)
+        print("Jogador 1 venceu a rodada")
+    # se o atributo do jogador 2 for maior do que o do jogador 1
+    elif mao_jogador1[0][atributo_j1] < mao_jogador2[0][atributo_j1]:
+        mao_jogador2.append(mao_jogador1[0])
+        mao_jogador1.pop(0)
+        print("Jogador 2 venceu a rodada")
+
+    
